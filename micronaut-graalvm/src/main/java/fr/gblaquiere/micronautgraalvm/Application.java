d6d4d1a2f9ccce99d467d2b14a99e738f06d07a7
Guillaume Blaquiere <guillaume.blaquiere@gmail.com>
2019-06-26 15:50:15 +0200

    Initial commit

---
+++ micronaut-graalvm/src/main/java/fr/gblaquiere/micronautgraalvm/Application.java
@@ -0,0 +1,14 @@
+package fr.gblaquiere.micronautgraalvm;
+
+import io.micronaut.context.env.Environment;
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.build(args)
+                .deduceEnvironment(false)
+                .environments(Environment.GOOGLE_COMPUTE)
+                .start();
+    }
+}
---
+++ micronaut-graalvm/src/main/java/fr/gblaquiere/micronautgraalvm/HelloWorld.java
@@ -0,0 +1,13 @@
+package fr.gblaquiere.micronautgraalvm;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/api")
+public class HelloWorld {
+
+    @Get("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
